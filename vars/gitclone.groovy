4267924e61a264ab1e5a7fffd1f0d546552380cc
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-05-15 18:56:19 +0530

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -1,14 +1,11 @@
 #!/usr/bin/env groovy
 
 
-stage("clone code") {
-                
-                    
-                    // Let's clone the source
-                    sh """ 
-                      git clone https://github.com/durgaprasad444/${APP_NAME}.git            
-                      cd ${APP_NAME}
-                      cp -r * /home/jenkins/workspace/maven-example
-                    """
-                }
+def call(body) {
+    echo "Disable balancer"
+
+   
+
+    return this
+}
          
