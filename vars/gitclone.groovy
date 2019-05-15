42631e0bc94ec48b144733146fbc483de3cf7351
durgaprasad444 <38001169+durgaprasad444@users.noreply.github.com>
2019-05-15 19:24:52 +0530

    Update gitclone.groovy

--- vars/gitclone.groovy
+++ vars/gitclone.groovy
@@ -5,6 +5,7 @@ def call(body) {
     echo "Disable balancer"
 
     node {
+        sh 'rm -rf hello-world'
         sh 'git clone https://github.com/durgaprasad444/hello-world.git'
         sh 'cd hello-world'
         sh 'cp -r * /var/lib/jenkins/workspace/target'
